4099c1b0fb55402f26f984a9632ac9e188ebcc2e
jessica.pei <jessica.pei@imaginuity.com>
2017-04-09 18:47:30 -0500

    style, add images

--- themes/eightythreec/front-page.php
+++ themes/eightythreec/front-page.php
@@ -34,17 +34,20 @@ get_header(); ?>
             $bigText = get_post_meta($post->ID, 'Big Text', false);
             $iAm = get_post_meta($post->ID, 'I am', true);
             $paragraph = get_post_meta($post->ID, 'Paragraph', false);
+            $background = get_post_meta($post->ID, 'Background', false);
             for ($x = 0; $x < count($bigText); $x++) { ?>
-                <div class="container-fluid">
+                <div class="container-fluid section-<?php echo $x;?>" style="background: url(<?php echo get_template_directory_uri() . '/img/' . $background[$x] ?>) center / cover no-repeat fixed">
                     <div class="container">
                         <div class="col-sm-12 col-md-7">
-							<span>
+                            <div class="vert-center">
+                                <span>
 								<?php echo $iAm; ?>
 							</span>
-                            <h2><?php echo $bigText[$x]; ?></h2>
-                            <p>
-                                <?php echo $paragraph[$x]; ?>
-                            </p>
+                                <h2><?php echo $bigText[$x]; ?></h2>
+                                <p>
+                                    <?php echo $paragraph[$x]; ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
